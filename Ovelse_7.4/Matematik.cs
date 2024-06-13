e9284f45b01c76e6542f0d8f0c1d720ece15298b
ChristofferOlsen2024 <ch.o@mailfence.com>
2024-06-13 07:55:56 +0200

    Add project files.

---
+++ Ovelse_7.4/Matematik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovelse_7._4
+{
+    internal class Matematik
+    {
+        public void UdskrivSummen(int tal1, int tal2)
+        {
+            Console.WriteLine($"Forskellen er {tal2 - tal1}");
+        }
+    }
+}
---
+++ Ovelse_7.4/Program.cs
@@ -0,0 +1,4 @@
+﻿using Ovelse_7._4;
+
+Matematik minMatematik = new();
+minMatematik.UdskrivSummen(3,4);
